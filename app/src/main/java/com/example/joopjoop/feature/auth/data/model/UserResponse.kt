c8166e59fe659f0b9d0bbaadbb74559002877583
yubin <vvvxx03@gmail.com>
2026-03-31 11:14:06 +0900

    User.kt와 변수명통일 (문제될수있어서...)

--- app/src/main/java/com/example/joopjoop/feature/auth/data/model/UserResponse.kt
+++ app/src/main/java/com/example/joopjoop/feature/auth/data/model/UserResponse.kt
@@ -7,6 +7,6 @@ data class UserResponse(
     val uid: String = "",
     val email: String = "",
     val nickname: String = "",
-    val profileImageBitmap: String?= null, // 이미지는 없을 수도 있으므로 Nullable
+    val profileImageUrl: String? = null, // 이미지는 없을 수도 있으므로 Nullable
     val createdAt: Long = 0L // 밀리초 단위로 저장할 경우
 )
