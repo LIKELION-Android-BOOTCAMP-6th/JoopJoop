154dd925d5c9a0476f15dd31df8331302a8290ee
jjh <jjh83301476@gmail.com>
2026-04-02 13:45:01 +0900

    중복 코드 오류 해결

--- app/src/main/java/com/example/joopjoop/core/di/AppContainer.kt
+++ app/src/main/java/com/example/joopjoop/core/di/AppContainer.kt
@@ -36,9 +36,7 @@ class AppContainer(context: Context) {
     // 인터페이스 타입으로 선언하여 외부(ViewModel)에서는 구현체를 몰라도 되게 함
     val authRepository: AuthRepository by lazy {
         AuthRepositoryImpl(firebaseAuthSource, firestoreUserSource, firestoreNoteSource)
-        AuthRepositoryImpl(firebaseAuthSource,
-            firestoreUserSource,
-            firestoreNoteSource)
+
     }
 
     // Note 리포지토리
