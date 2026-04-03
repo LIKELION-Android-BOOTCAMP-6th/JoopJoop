b5d6a3aad689bd61023509b030c5affa8f99b299
eunsin_seo <heynunnu52@gmail.com>
2026-04-03 10:18:56 +0900

    회원가입 화면에서 스크롤 가능하게 수정

--- app/src/main/java/com/example/joopjoop/feature/auth/ui/signup/SignupScreen.kt
+++ app/src/main/java/com/example/joopjoop/feature/auth/ui/signup/SignupScreen.kt
@@ -14,7 +14,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -144,6 +146,7 @@ fun SignupScreen(
                 .padding(innerPadding)
                 .padding(horizontal = 24.dp)
                 .fillMaxSize()
+                .verticalScroll(rememberScrollState())
         ) {
             Spacer(modifier = Modifier.height(16.dp))
 
